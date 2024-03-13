2cc4c2c1a50d3299b29c801491b8b6dc4588ac04
andrewchristy <andrew.christy@defra.gov.uk>
2024-03-13 17:57:00 +0000

    Updates escription and example for get_table_id function

--- R/get_table_id.R
+++ R/get_table_id.R
@@ -1,5 +1,5 @@
-#' Extract unique table id - this is used to 
-#' rapidly query census data in other functions
+#' Extract unique table ids  for tables containing given name in their title
+#' - these unique table ids are used to rapidly query census data in other functions
 #' 
 #' @import xml2
 #' 
@@ -7,8 +7,8 @@
 #' 
 #' @param name A valid NOMIS table name given as a string
 #' 
-#' @examples get_table_id(name="RM003 - Accommodation type by type of central heating in household by tenure")
-#' @returns The unique table code that allows rapid querying of the data e.g. "NM_102_1"
+#' @examples get_table_id(name="employment")
+#' @returns The nomis table code(s) for all tables containing the selected name, that allows rapid querying of the data e.g. "NM_102_1"
 #' @export
 
 get_table_id <- function(name){
@@ -24,7 +24,7 @@ get_table_id <- function(name){
   assert_function(length(raw_data$structure$keyfamilies)==2L,"No tables with chosen name")
   
   num_files=length(raw_data$structure$keyfamilies$keyfamily)
-  message(paste0(num_files," table names contain your selected charater string"))
+  message(paste0(num_files," table names contain your selected character string"))
   d_rows <- data.frame()
   for (i in seq_along(raw_data$structure$keyfamilies$keyfamily)) {
     d_row <- data.frame(dn = i,
