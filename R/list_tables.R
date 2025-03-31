1012340148a78f6006b761e95c011d153b3a3c77
Michael Stocks <michael.stocks@defra.gov.uk>
2025-03-31 14:16:24 +0100

    Add tests for list_tables & simplify logic
    The logic is not vectorized and uses a loop where base R functions
    would perhaps be preferable. This commit simplifies this logic and adds
    more test cases that cover whether the data has successfully be
    fetched and converted into a data frame

--- R/list_tables.R
+++ R/list_tables.R
@@ -15,16 +15,10 @@
 list_tables <- function(base_url = "https://www.nomisweb.co.uk/api/v01/"){
   
   y <- httr::GET("https://www.nomisweb.co.uk/api/v01/dataset/def.sdmx.json") %>%
-    httr::content() 
-  
-  available_datasets <- data.frame()
-  for(i in 1:length(y$structure$keyfamilies$keyfamily)){
-    available_datasets <- rbind(available_datasets,
-                               data.frame(
-                                 name = y$structure$keyfamilies$keyfamily[[i]]$name$value,
-                                 id = y$structure$keyfamilies$keyfamily[[i]]$id)
-    )
-  }
-  
-  return(available_datasets)
+    httr::content()
+
+	nomis_names <- unlist(map(y$structure$keyfamilies$keyfamily, function (x) x[["name"]]$value))
+	nomis_ids <- unlist(map(y$structure$keyfamilies$keyfamily, function (x) x[["id"]]))
+
+	return(data.frame(name = nomis_names, id = nomis_ids))
 }
