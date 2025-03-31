b1679551465f9880a2b0bd8b8664b4d73e9d2c48
Michael Stocks <michael.stocks@defra.gov.uk>
2025-03-31 15:10:10 +0100

    Add tests for list_boundaries & simplify logic
    The logic is not vectorized and uses a loop where base R functions
    would perhaps be preferable. This commit simplifies this logic and adds
    more test cases that cover whether the data has successfully be
    fetched and converted into a data frame
    
    All tests in tests/testhat/test-list_boundaries.R pass

--- R/list_boundaries.R
+++ R/list_boundaries.R
@@ -13,18 +13,9 @@
 list_boundaries <- function(){
   
   base_url = "https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/"
-  
-  raw_data <- {httr::GET(paste0(base_url,
-                               "?f=pjson"))%>%
-    httr::content()}$services
-  
-  boundaries <- c()
-  
-  for(i in raw_data){
-    boundaries <- c(boundaries, i[["name"]])
-    
-  }
-  return(boundaries)
+  raw_data <- httr::GET(paste0(base_url, "?f=pjson")) %>% httr::content()
+	
+	return(unlist(map(raw_data$services, function(x) x[["name"]])))
 }
 
 
