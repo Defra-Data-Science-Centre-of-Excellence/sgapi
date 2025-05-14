131d1c740451c2fde065307e62811988d269f7dd
Michael Stocks <michael.stocks@defra.gov.uk>
2025-05-14 09:52:24 +0100

    Single list_ons_tables fnc for listing and searching #7
    Currently, there are two functions (list_tables & get_keyword_table_id)
    for retrieving ONS table information. Furthermore, the
    search functionality can only allow keyword searches. This
    commit:
    
    - Adds a new function (list_ons_tables) that allows
      users to either return the full list of tables
      (by omitting the 'search' argument) or a subset
      of tables by providing one or more search
      expressions via the 'search' argument
    
    - The search feature is extended to cover all the
      functionality provided by the nomis API
    
    - The json output is parsed directly by jsonlite (rather
      than indirectly through httr) removing a dependency and
      making the logic simpler

---
+++ R/list_ons_tables.R
@@ -0,0 +1,49 @@
+#' @title List all or search ONS data
+#'
+#' @description
+#' List all ONS datasets available through the nomis API or
+#' search for datasets matching specific search terms
+#' 
+#' @import jsonlite
+#'
+#' @param search Search for a dataset by providing a string 'term' where "*" is
+#' a wildcard (e.g. "*claimant*"). Searches can be refined to just the name, 
+#' description or keywords (e.g. "name-*claimant*", "description-*jobseeker*"). 
+#' Matches are not case-sensitive. If NULL then all tables are returned. 
+#' See 'Discover available datasets' section of https://nomisweb.co.uk/api/v01/help
+#' for details. Default: NULL
+#' @param base_url Url of the API from which the available tables are listed.
+#'
+#' @examples
+#' # Retrieve all available ONS datasets
+#' list_ons_tables()
+#'
+#' # Search for a given search term within name, description or keywords
+#' list_ons_tables(search = "*claimants*")
+#'
+#' # Refine search to specific fields, or add multiple search terms
+#' list_ons_tables(search = "description-*jobseeker*")
+#' list_ons_tables(search = "name-*jobseeker*,*age*")
+#'
+#' @returns A dataframe containing the name and ID of each table available on 'nomis'.
+#' 
+#' @export
+
+list_ons_tables <- function(search = NULL, base_url = "https://www.nomisweb.co.uk/api/v01"){
+  nomis_url <- paste0(base_url, "/dataset/def.sdmx.json")
+  
+  if (!is.null(search)) {
+    nomis_url <- sprintf("%s?search=%s", nomis_url, search)
+  }
+
+  message("Querying Nomis API -> ", nomis_url)
+   
+  nomis_json <- jsonlite::fromJSON(nomis_url)
+  nomis_table <- nomis_json$structure$keyfamilies$keyfamily
+  
+  if (is.null(nomis_table)) {
+    warning(sprintf("No nomis tables matched search '?search=%s'", search))
+  }
+
+  return(data.frame(id = nomis_table$id, name = nomis_table$name$value))
+}
